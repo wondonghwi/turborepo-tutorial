4f48cf9b3eab578adeace04f88377968b0fcabab
wondonghwi <ehdehd1289@naver.com>
2024-02-15 01:37:42 +0900

    feat: 모노레포 vercel 배포 에러 수정 v8

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -80,7 +80,7 @@ export default function Page(): JSX.Element {
       <Button
         appName='web'
         className={styles.button}>
-        Click web jarry!
+        Click web jarry
       </Button>
 
       <div className={styles.hero}>
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -11,6 +11,8 @@ interface ButtonProps {
 console.log('여기 로그추가!');
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
+  console.log(appName);
+
   return (
     <button
       className={className}
