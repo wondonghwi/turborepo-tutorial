0ec6c38dc1d5c01ca24e6145ded3b1f65ab4c02c
wondonghwi <ehdehd1289@naver.com>
2024-02-15 01:34:08 +0900

    feat: 모노레포 vercel 배포 에러 수정 v7

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
   appName: string;
 }
 
-console.log('여기 로그추가');
+console.log('여기 로그추가!');
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
